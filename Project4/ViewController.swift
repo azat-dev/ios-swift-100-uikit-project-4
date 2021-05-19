cb04a88e55e2d82fd7cdeeeb359afafb5baffb97
Azat <kaumov.a.r@gmail.com>
2021-05-19 21:52:46 +0300

    Add ability to select the page

--- Project4/ViewController.swift
+++ Project4/ViewController.swift
@@ -8,21 +8,87 @@
 import UIKit
 import WebKit
 
+struct PageInfo {
+    let title: String
+    let link: String
+}
+
 class ViewController: UIViewController, WKNavigationDelegate {
     var webView: WKWebView!
+    
+    let pages: [PageInfo] = [
+        .init(title: "google.com", link: "https://google.com"),
+        .init(title: "apple.com", link: "https://apple.com"),
+        .init(title: "hackingwithswift.com", link: "https://hackingwithswift.com"),
+    ]
     
     override func loadView() {
         webView = WKWebView()
         webView.navigationDelegate = self
         view = webView
     }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    
+    func initWebView() {
         let url = URL(string: "https://google.com")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
+    }
+    
+    func initNavigationBar() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "Open",
+            style: .plain,
+            target: self,
+            action: #selector(openButtonTapped)
+        )
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        initWebView()
+        initNavigationBar()
+    }
+    
+    @objc func openButtonTapped() {
+        let alert = UIAlertController(
+            title: "Open page...",
+            message: nil,
+            preferredStyle: .actionSheet
+        )
+        
+        for page in pages {
+            alert.addAction(
+                .init(
+                    title: page.title,
+                    style: .default,
+                    handler: openPage
+                )
+            )
+        }
+        
+        alert.addAction(
+            .init(
+                title: "Cancel",
+                style: .cancel
+            )
+        )
+        
+        alert.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        
+        present(alert, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        guard let page = pages.first(where: { $0.title == action.title }) else {
+            return
+        }
+        
+        guard let url = URL(string: page.link) else {
+            return
+        }
+        
+        webView.load(URLRequest(url: url))
     }
 }
 
