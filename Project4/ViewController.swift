9bce64b6a8bfb4a2a9fbb396ed50c4c2355301f2
Azat <kaumov.a.r@gmail.com>
2021-05-19 20:59:52 +0300

    Initial commit

---
+++ Project4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by Azat Kaiumov on 19.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
